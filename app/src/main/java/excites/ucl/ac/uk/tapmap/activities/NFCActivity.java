6b5cb0a2147bccfdda557b8d3c406f2d65cb1be5
Michalis Vitos <info@michalisvitos.gr>
2018-05-11 16:16:08 +0100

    Add NFC Activity
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

---
+++ app/src/main/java/excites/ucl/ac/uk/tapmap/activities/NFCActivity.java
@@ -0,0 +1,14 @@
+package excites.ucl.ac.uk.tapmap.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import excites.ucl.ac.uk.tapmap.R;
+
+public class NFCActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_nfc);
+  }
+}
