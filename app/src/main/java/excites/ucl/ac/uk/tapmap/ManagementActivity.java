c35d5a0b849594cad38c06329b2570f5b33138b2
Michalis Vitos <info@michalisvitos.gr>
2018-05-18 16:11:01 +0100

    Improve UI on ManagementActivity.java
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- app/src/main/java/excites/ucl/ac/uk/tapmap/ManagementActivity.java
+++ app/src/main/java/excites/ucl/ac/uk/tapmap/ManagementActivity.java
@@ -3,10 +3,10 @@ package excites.ucl.ac.uk.tapmap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import excites.ucl.ac.uk.tapmap.activities.NFCActivity;
+import excites.ucl.ac.uk.tapmap.activities.ManageNfcCardsActivity;
+import excites.ucl.ac.uk.tapmap.activities.TapAndMapActivity;
 import timber.log.Timber;
 
 public class ManagementActivity extends AppCompatActivity {
@@ -16,16 +16,25 @@ public class ManagementActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_managment);
     ButterKnife.bind(this);
-    // TODO Use fields...
 
     Timber.d("Hi from the ManagementActivity");
+
+    //openNFCActivity();
   }
 
-  @OnClick(R.id.btn_goToNFC)
+  @OnClick(R.id.btn_collectData)
   public void openNFCActivity() {
     Timber.d("Go to NFC activity");
 
-    Intent intent = new Intent(this, NFCActivity.class);
+    Intent intent = new Intent(this, TapAndMapActivity.class);
+    startActivity(intent);
+  }
+
+  @OnClick(R.id.btn_manageCards)
+  public void openManageNfcCardsActivity() {
+    Timber.d("Go to NFC activity");
+
+    Intent intent = new Intent(this, ManageNfcCardsActivity.class);
     startActivity(intent);
   }
 }
