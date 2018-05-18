5788f66d5cc502e41b390bddbcc1a4ad19805f5a
Michalis Vitos <info@michalisvitos.gr>
2018-05-18 16:41:34 +0100

    WIP: Show icons on the NFC touch on TapAndMapActivity.java
    Signed-off-by: Michalis Vitos <info@michalisvitos.gr>

--- app/src/main/java/excites/ucl/ac/uk/tapmap/activities/TapAndMapActivity.java
+++ app/src/main/java/excites/ucl/ac/uk/tapmap/activities/TapAndMapActivity.java
@@ -1,12 +1,23 @@
 package excites.ucl.ac.uk.tapmap.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import excites.ucl.ac.uk.tapmap.R;
 import excites.ucl.ac.uk.tapmap.nfc.NfcCard;
+import excites.ucl.ac.uk.tapmap.nfc.NfcManagement;
+import java.io.File;
 import timber.log.Timber;
 
 public class TapAndMapActivity extends NfcBaseActivity {
+
+  private static final int THUMBNAIL_SIZE = 64;
+
+  @BindView(R.id.nfc)
+  protected ImageView nfcImageView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,13 @@ public class TapAndMapActivity extends NfcBaseActivity {
   protected void handleNfcCard(NfcCard nfcCard) {
 
     Timber.d(nfcCard.toString());
+
+    final String imagePath = NfcManagement.getInstance().getImagePath(nfcCard);
+    if (imagePath != null && !imagePath.isEmpty()) {
+
+      File file = new File(imagePath);
+      Bitmap pickedIcon = BitmapFactory.decodeFile(file.getPath());
+      nfcImageView.setImageBitmap(pickedIcon);
+    }
   }
 }
