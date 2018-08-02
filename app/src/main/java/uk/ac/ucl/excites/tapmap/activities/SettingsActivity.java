932f0d97801dbddbb5b55e81e415d25eb0d9b988
Michalis Vitos <info@michalisvitos.gr>
2018-08-02 13:35:56 +0300

    Add screen settings

--- app/src/main/java/uk/ac/ucl/excites/tapmap/activities/SettingsActivity.java
+++ app/src/main/java/uk/ac/ucl/excites/tapmap/activities/SettingsActivity.java
@@ -1,7 +1,8 @@
 package uk.ac.ucl.excites.tapmap.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 import uk.ac.ucl.excites.tapmap.R;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -9,6 +10,16 @@ public class SettingsActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_settings);
+    getFragmentManager().beginTransaction()
+        .replace(android.R.id.content, new SettingsFragment())
+        .commit();
+  }
+
+  public static class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.preferences);
+    }
   }
 }
